726e09aaf8f48d2837b368fcabb0c169768487e1
serkancan <serkancan@DESKTOP-9CS0Q1K>
2024-02-07 00:54:09 +0300

    düzenleme

--- OkyanusWebAPI/AdminControllers/AboutDenemeController.cs
+++ OkyanusWebAPI/AdminControllers/AboutDenemeController.cs
@@ -1,28 +1,47 @@
 ﻿//using AutoMapper;
+//using FluentValidation;
 //using Microsoft.AspNetCore.Http;
 //using Microsoft.AspNetCore.Mvc;
 //using Okyanus.BusinessLayer.Abstract;
+//using Okyanus.BusinessLayer.ValidationRules;
+//using Okyanus.EntityLayer.Entities;
 
-//namespace OkyanusWebAPI.AdminControllers
+//namespace OkyanusWebAPI.Controllers
 //{
 //    [Route("api/admin/[controller]")]
 //    [ApiController]
 //    public class AboutDenemeController : ControllerBase
 //    {
-//        //private readonly IAboutService _aboutService;
-//        //private readonly IMapper _mapper;
+//        private readonly IAboutService _aboutService;
+//        private readonly IMapper _mapper;
+//        private readonly IValidator<About> _validator;
 
-//        //public AboutDenemeController(IAboutService aboutService, IMapper mapper)
-//        //{
-//        //    _aboutService = aboutService;
-//        //    _mapper = mapper;
-//        //}
+//        public AboutDenemeController(IAboutService aboutService, IMapper mapper, IValidator<About> validator)
+//        {
+//            _aboutService = aboutService;
+//            _mapper = mapper;
+//            _validator = validator;
+//        }
 
-//        //[HttpGet]
-//        //public IActionResult Index()
-//        //{
-//        //    var values = _aboutService.TWhere(x => x.Status == true).ToList();
-//        //    return Ok(values);
-//        //}
+//        [HttpGet]
+//        public IActionResult Index()
+//        {
+//            [HttpPost] için yapılır... Index(About about)
+//            if (!ModelState.IsValid)
+//            {
+//                return BadRequest(ModelState);
+//            }
+//
+//            var validator = new AboutValidators();
+//            var validationResult = validator.Validate(about);
+//            //var validationResult = _validator.Validate(about);
+
+//            if (!validationResult.IsValid)
+//            {
+//                return BadRequest(validationResult.Errors);
+//            }
+//            var values = _aboutService.TWhere(x => x.Status == true).ToList();
+//            return Ok(values);
+//        }
 //    }
 //}
