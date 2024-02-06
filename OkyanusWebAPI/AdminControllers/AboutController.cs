f9a0207a6dcc7b1ecdb298f0e31b127b4c603623
serkancan <serkancan@DESKTOP-9CS0Q1K>
2024-02-06 23:34:26 +0300

    farklı metorolojiler

---
+++ OkyanusWebAPI/AdminControllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OkyanusWebAPI.AdminControllers
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult actionResult()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ OkyanusWebAPI/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OkyanusWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult actionResult()
+        {
+            return Ok();
+        }
+    }
+}
