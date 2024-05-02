77f894b87e07d0d7ddbf6a5c2101c8409cc5ba1b
Serkan Can Kaya <109299838+serkancan006@users.noreply.github.com>
2024-05-02 09:46:16 +0300

    product detail modal duzenlemesi

---
+++ OkyanusWebUI/ViewComponents/_OrderDetailModalPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OkyanusWebUI.Models.ProductVM;
+using OkyanusWebUI.Service;
+
+namespace OkyanusWebUI.ViewComponents
+{
+    public class _OrderDetailModalPartial : ViewComponent
+    {
+        private readonly CustomHttpClient _customHttpClient;
+        public _OrderDetailModalPartial(CustomHttpClient customHttpClient)
+        {
+            _customHttpClient = customHttpClient;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int productID)
+        {
+            var responseMessage = await _customHttpClient.Get(new() { Controller = "Product" }, productID);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultProductVM>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
