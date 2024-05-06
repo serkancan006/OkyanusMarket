5046c4940966db05ae2b95338d44f067ad2bde56
Serkan Can Kaya <109299838+serkancan006@users.noreply.github.com>
2024-05-06 17:42:22 +0300

    ...

--- OkyanusWebUI/Areas/Admin/Models/AdminGroupVM/CreateGroupVM.cs
+++ OkyanusWebUI/Areas/Admin/Models/AdminGroupVM/CreateGroupVM.cs
@@ -7,6 +7,6 @@
         public string ALTGRUP2 { get; set; } 
         public string ALTGRUP3 { get; set; } 
         public string GRUPADI { get; set; }
-        //public string? Description { get; set; }
+        public string? Description { get; set; }
     }
 }
--- OkyanusWebUI/Areas/Admin/Models/AdminGroupVM/ResultGroupVM.cs
+++ OkyanusWebUI/Areas/Admin/Models/AdminGroupVM/ResultGroupVM.cs
@@ -7,7 +7,7 @@
         public string ALTGRUP2 { get; set; } 
         public string ALTGRUP3 { get; set; } 
         public string GRUPADI { get; set; }
-        //public string? Description { get; set; }
+        public string? Description { get; set; }
 
     }
 }
